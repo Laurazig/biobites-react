56f9d849da61d8504592fcc391f26e72b52679e7
Laura Hesse <lhzig@protonmail.com>
2023-09-17 21:01:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DCI final BIOBITES clone
+
+Using tutorial:
+https://www.youtube.com/watch?v=8DploTqLstE&list=PL4cUxeGkcC9iJ_KkrkBZWZRHVwnzLIoUE&index=2
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//need to install reac-router-dom from react 6
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        <h1>Biobites</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
