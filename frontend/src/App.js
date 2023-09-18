38d46758e80ae31a58ee5bd5d73647d32fa03ef7
Laura Hesse <lhzig@protonmail.com>
2023-09-18 21:38:26 +0200

    added react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 
 import './App.css';
 //need to install reac-router-dom from react 6
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import CartPage from "./views/cartPage/CartPage";
+import MealsPage from "./views/mealsPage/MealsPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <body>
-        <h1>Biobites</h1>
-      </body>
-    </div>
+<div className='App'>
+        <Router>
+          <Routes>
+            <Route path="/meals" element={<MealsPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <h1>Biobites</h1>
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
