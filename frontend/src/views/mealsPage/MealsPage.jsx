999d922a2ee907a472152d95bcb7e41a716235e7
Laura Hesse <lhzig@protonmail.com>
2023-09-19 20:42:06 +0200

    adding assets and local data

--- frontend/src/views/mealsPage/MealsPage.jsx
+++ frontend/src/views/mealsPage/MealsPage.jsx
@@ -1,8 +1,10 @@
 const MealsPage = () => {
 
-    return(
+    return (
         <div>
-
+            <div>
+                {/* <button onClick={() => addToCart(meal)}>Add To Cart</button> */}
+            </div>
         </div>
     )
 }
