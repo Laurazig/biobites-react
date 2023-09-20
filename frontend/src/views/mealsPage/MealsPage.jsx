f90e4ae9c2a5d44f502529ee6ca00765f63d6987
Laura Hesse <lhzig@protonmail.com>
2023-09-20 22:06:35 +0200

    accessing local meal data

--- frontend/src/views/mealsPage/MealsPage.jsx
+++ frontend/src/views/mealsPage/MealsPage.jsx
@@ -1,8 +1,12 @@
-const MealsPage = () => {
 
+// import meals from "../../../../server/meals.json"
+const MealsPage = () => {
+    let data = require("./meals.json")
+    //map
     return (
         <div>
             <div>
+{data.title}
                 {/* <button onClick={() => addToCart(meal)}>Add To Cart</button> */}
             </div>
         </div>
