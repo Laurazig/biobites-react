4aadb171af54e3f6c7893be38da12af061a01439
Laura Hesse <lhzig@protonmail.com>
2023-09-27 21:54:51 +0200

    adding to cart page

--- frontend/src/views/cartPage/CartPage.jsx
+++ frontend/src/views/cartPage/CartPage.jsx
@@ -1,9 +1,28 @@
 const CartPage = () => {
 
+    //useContext
+
+    //useNavigate
+
+    //useState
+
+    // useEffect(() => {})
+
+    // const submitOrder = async (e) => {
+    //     e.preventDefault();
+    // };
+
+    // const deleteSingleOrderedMeal = (meal) => {
+    // }
+
+    // const stripe = async () => {}
     return (
         <div>
             <h2>Cart</h2>
             <h3>Selected meals will show here</h3>
+            {/* show meals - delete option */}
+            {/* buy now - stripe */}
+            
         </div>
     )
 }
